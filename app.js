a38496a4cd8de42f557af7c9bab2f2433309cc06
jmm <martin.juanmiguel@gmail.com>
2023-03-03 12:39:48 -0300

    add button to create cookie

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var sendCookieRouter = require('./routes/sendCookies');
-var createCookieRouter = require('./routes/createCookies');
 
 var cors = require('cors');
 var app = express();
+app.use(cors({
+  origin:[' https://19f2-2803-9800-909f-80fc-69f8-103c-d1d-9dd1.sa.ngrok.io','https://465f-2803-9800-909f-80fc-69f8-103c-d1d-9dd1.sa.ngrok.io']  ,// callback expects two parameters: error and options,
+  credentials: true, // Permitir el intercambio de cookies
+  // exposedHeaders: ['Content-Length', 'Authorization', 'X-Powered-By'], // Cabeceras expuestas en la respuesta
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos en la solicitud
+  // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'] // Cabeceras permitidas en la solicitud
+}));
 
+// Configuración de cabeceras "Forwarded" y "X-Forwarded-For"
+//app.set('trust proxy', true);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,30 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var allowedOrigins = ['https://aa52-2803-9800-909f-80fc-71aa-e3e7-c5c2-33ed.sa.ngrok.io',//3001
-                      'https://fe4e-2803-9800-909f-80fc-71aa-e3e7-c5c2-33ed.sa.ngrok.io'];//3002;
-// app.use(cors({
-//   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
-//   //credentials: true,
-//  // origin: 
-//   // function(origin, callback){
-//   //   // allow requests with no origin 
-//   //   // (like mobile apps or curl requests)
-//   //   if(!origin) return callback(null, true);
-//   //   if(allowedOrigins.indexOf(origin) === -1){
-//   //     var msg = 'The CORS policy for this site does not ' +
-//   //               'allow access from the specified Origin.';
-//   //     return callback(new Error(msg), false);
-//   //   }
-//   //   return callback(null, true);
-//   // }
-// }));
-
-
-
 app.use('/', indexRouter);
-//app.use('/sendCookies', sendCookieRouter);
-//app.use('/createCookies', createCookieRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -62,21 +46,15 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(cors({
-  origin:allowedOrigins,
-  credentials: true,
- // origin: 
-  // function(origin, callback){
-  //   // allow requests with no origin 
-  //   // (like mobile apps or curl requests)
-  //   if(!origin) return callback(null, true);
-  //   if(allowedOrigins.indexOf(origin) === -1){
-  //     var msg = 'The CORS policy for this site does not ' +
-  //               'allow access from the specified Origin.';
-  //     return callback(new Error(msg), false);
-  //   }
-  //   return callback(null, true);
-  // }
-}));
+// var allowedOrigins = ['https://aa52-2803-9800-909f-80fc-71aa-e3e7-c5c2-33ed.sa.ngrok.io',//3001
+//                       'https://fe4e-2803-9800-909f-80fc-71aa-e3e7-c5c2-33ed.sa.ngrok.io'];//3002;
+// app.use(cors({
+//   origin:['http://localhost:3001/','http://localhost:3002/'] ,// callback expects two parameters: error and options,
+//   credentials: true,
+//  // allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+ 
+// }));
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.cookie('multidomainCookie', 'valueCookie')
-  res.render('index', { title: 'Express app One' });
+  res.render('index', { title: 'Share Cookie app One' });
 });
+/* GET home page. */
+router.get('/createCookie', function(req, res, next) {
+  console.log('aca en create')
+  res.set('Access-Control-Allow-Credentials', 'true');
+  res.set('Access-Control-Allow-Origin', 'https://8dd5-2803-9800-909f-80fc-69f8-103c-d1d-9dd1.sa.ngrok.io');
+  res.cookie('multidomainCookie', 'valueCookie',{sameSite:'None',secure:true}) //,secure:true
+  res.end();
+});
+
 
 module.exports = router;
